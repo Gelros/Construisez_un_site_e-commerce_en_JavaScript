ef65edebffe923909aa3b7f116df8e245de38ed7
ConstruisezUneAPI <guillaume.lacolley@gmail.com>
2023-06-29 21:58:50 +0200

    ajustement

--- back/app.js
+++ back/app.js
@@ -4,17 +4,13 @@ const path = require('path');
 const productRoutes = require('./routes/product');
 
 const app = express();
-const cors = require("cors")
 
-const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use(express.static('images'));
